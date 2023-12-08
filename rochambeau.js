f3b553cfd59f66030cbe71536d082495c2befb6e
sumusa <physiosummy@gmail.com>
2023-12-08 16:13:13 -0330

    include print output variables

--- rochambeau.js
+++ rochambeau.js
@@ -14,13 +14,13 @@ function playRound(playerSelection, computerSelection) {
       (playerSelection === 'paper' && computerSelection === 'Rock') ||
       (playerSelection === 'scissors' && computerSelection === 'Paper')
     ) {
-      return 'You Win! ${playerSelection} beats ${computerSelection}';
+      return `You Win! ${playerSelection} beats ${computerSelection}`;
     } else if (
       (playerSelection === 'scissors' && computerSelection === 'Rock') ||
       (playerSelection === 'rock' && computerSelection === 'Paper') ||
       (playerSelection === 'paper' && computerSelection === 'Scissors')
     ) {
-      return 'You Lose! ${computerSelection} beats ${playerSelection}';
+      return `You Lose! ${computerSelection} beats ${playerSelection}`;
     } else {
       return "It's a Tie! Play again.";
     }
